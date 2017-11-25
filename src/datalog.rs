5e00f3f8bec0c9c3babcec1030cf267959613c10
Matthew Maurer <matthew.r.maurer@gmail.com>
2017-11-25 09:40:45 -0500

    Use new shortcut fields syntax

--- src/datalog.rs
+++ src/datalog.rs
@@ -146,8 +146,8 @@ func_uses {
 ?call_site: call_site {
     call_file: call_file,
     call_addr: call_addr,
-    dst_file: dst_file,
-    dst_addr: dst_addr
+    dst_file,
+    dst_addr
 }
 ?succ: succ {
     file_name: name,
@@ -168,26 +168,26 @@ func_uses {
 ?live: live {file_name: name, addr: addr}
 
 bap_dump_segments: segment {
-    file_name: file_name,
+    file_name,
     contents: seg_contents,
-    start: start,
-    end: end,
-    read: read,
-    write: write,
-    execute: execute
-} <- file {name: file_name, contents: contents} + ::funcs::dump_segments
+    start,
+    end,
+    read,
+    write,
+    execute
+} <- file {name: file_name, contents} + ::funcs::dump_segments
 objdump_dump_plt: link_pad {
-    file_name: file_name,
-    pad_name: pad_name,
-    pad_addr: pad_addr
-} <- file {name: file_name, contents: contents} + ::funcs::dump_plt
+    file_name,
+    pad_name,
+    pad_addr
+} <- file {name: file_name, contents} + ::funcs::dump_plt
 bap_dump_syms: sym {
-    file_name: file_name,
-    name: name,
-    start: start,
-    end: end
-} <- file {name: file_name, contents: contents} + ::funcs::dump_syms
-syms_live: live {file_name: file_name, addr: addr} <- sym {file_name: file_name, start: addr}
+    file_name,
+    name,
+    start,
+    end
+} <- file {name: file_name, contents} + ::funcs::dump_syms
+syms_live: live {file_name, addr} <- sym {file_name: file_name, start: addr}
 bap_sema: lift {
     file_name: file_name,
     address: addr,
@@ -197,9 +197,9 @@ bap_sema: lift {
     is_call: call,
     is_ret: ret
 } <- live {file_name: file_name, addr: addr} & segment {file_name: file_name, contents: seg_contents, start: seg_start, end: seg_end} & prog_arch {file_name: file_name, arch: arch} + ::funcs::lift
-sema_succ: succ {file_name: file_name, src_addr: src_addr, dst_addr: dst_addr, is_call: is_call} <- lift {file_name: file_name, address: src_addr, bil: bil, fallthrough: fall, is_call: is_call} + ::funcs::sema_succ
+sema_succ: succ {file_name: file_name, src_addr: src_addr, dst_addr, is_call} <- lift {file_name: file_name, address: src_addr, bil: bil, fallthrough: fall, is_call} + ::funcs::sema_succ
 skip_computed_call: succ {file_name: file_name, src_addr: src_addr, dst_addr: fall_addr, is_call: ~false} <- lift {file_name: file_name, address: src_addr, fallthrough: fall_addr, is_call: ~true, bil: bil} + ::funcs::is_computed_jump
-live_succ_live: live { file_name: file_name, addr: dst_addr } <- succ {file_name: file_name, src_addr: src_addr, dst_addr: dst_addr} & live {file_name: file_name, addr: src_addr}
+live_succ_live: live { file_name: file_name, addr: dst_addr } <- succ {file_name: file_name, src_addr: src_addr, dst_addr} & live {file_name: file_name, addr: src_addr}
 live_call_live: live { file_name: file_name, addr: fall } <- lift { file_name: file_name, fallthrough: fall, is_call: ~true }
 bap_arch: prog_arch { file_name: file_name, arch: arch } <- file {name: file_name, contents: contents} + ::funcs::get_arch
 malloc_call_by_name: malloc_call {file_name: file_name, addr: addr} <- link_pad { file_name: file_name, pad_name: func_name, pad_addr: tgt_addr } & succ { file_name: file_name, src_addr: addr, dst_addr: tgt_addr, is_call: ~true } + ::funcs::is_malloc_name
@@ -207,10 +207,10 @@ free_call_by_name: free_call {file_name: file_name, addr: addr} <- link_pad { fi
 puts_uses: func_uses {file_name: name, addr: addr, var: ~(::avar::get_arg0())} <- link_pad {file_name: name, pad_name: ~("puts".to_string()), pad_addr: tgt} & succ {file_name: name, src_addr: addr, dst_addr: tgt }
 skip_dyn: skip_func { file_name: name, addr: addr } <- link_pad { file_name: name, pad_addr: tgt } & succ {file_name: name, src_addr: addr, dst_addr: tgt, is_call: ~true}
 
-succ_over_normal: succ_over { file_name: name, src: src, dst: dst } <- succ { file_name: name, src_addr: src, dst_addr: dst, is_call: ~false }
-succ_over_skip_call: succ_over { file_name: name, src: src, dst: dst } <- succ { file_name: name, src_addr: src, is_call: ~true } & lift { file_name: name, address: src, fallthrough: dst }
-call_site_internal: call_site { call_file: name, call_addr: src_addr, dst_file: name, dst_addr: dst_addr } <- succ { file_name: name, src_addr: src_addr, dst_addr: dst_addr, is_call: ~true }
-call_site_dyn: call_site { call_file: src_name, call_addr: src_addr, dst_file: dst_name, dst_addr: dst_addr } <- succ { file_name: name, src_addr: src_addr, dst_addr: pad_addr, is_call: ~true } & link_pad {file_name: src_name, pad_name: func_name, pad_addr: pad} & sym { file_name: dst_name, name: func_name, start: dst_addr }
+succ_over_normal: succ_over { file_name: name, src: src, dst } <- succ { file_name: name, src_addr: src, dst_addr: dst, is_call: ~false }
+succ_over_skip_call: succ_over { file_name: name, src: src, dst } <- succ { file_name: name, src_addr: src, is_call: ~true } & lift { file_name: name, address: src, fallthrough: dst }
+call_site_internal: call_site { call_file: name, call_addr: src_addr, dst_file: name, dst_addr } <- succ { file_name: name, src_addr: src_addr, dst_addr, is_call: ~true }
+call_site_dyn: call_site { call_file: src_name, call_addr: src_addr, dst_file: dst_name, dst_addr } <- succ { file_name: name, src_addr: src_addr, dst_addr: pad_addr, is_call: ~true } & link_pad {file_name: src_name, pad_name: func_name, pad_addr: pad} & sym { file_name: dst_name, name: func_name, start: dst_addr }
 func_start: func {file_name: name, entry: addr, addr: addr } <- sym { file_name: name, start: addr }
 func_walk_over: func {file_name: name, entry: entry, addr: next } <- func {file_name: name, entry: entry, addr: addr } & succ_over {file_name: name, src: addr, dst: next}
 flow_start_malloc: path_alias {initial_file: file, initial_addr: addr, alias_set: ~ZERO, stack: ~(new_stack()), chop: ~(new_chop()), current_file: file, current_addr: step, aliased_var: ~(::avar::get_ret()), freed: ~false} <- malloc_call {file_name: file, addr: addr} & lift {file_name: file, address: addr, fallthrough: step}
