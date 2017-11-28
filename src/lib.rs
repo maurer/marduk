9ae5c43fc422d9fa4f1ace98fac0afb7537e6d8b
Matthew Maurer <matthew.r.maurer@gmail.com>
2017-11-28 02:14:30 -0500

    Move printers to their own area

--- src/lib.rs
+++ src/lib.rs
@@ -8,86 +8,9 @@ extern crate num_traits;
 pub mod avar;
 pub mod datalog;
 pub mod funcs;
+mod printers;
 
 pub use datalog::Database;
-
-impl ::std::fmt::Display for datalog::FuncResult {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
-        write!(f, "{}@{}:{}", self.file, self.entry, self.addr)
-    }
-}
-
-impl ::std::fmt::Display for datalog::CallSiteResult {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
-        write!(
-            f,
-            "{}@{}->{}@{}",
-            self.call_file,
-            self.call_addr,
-            self.dst_file,
-            self.dst_addr
-        )
-    }
-}
-
-impl ::std::fmt::Display for datalog::GetUafFlowResult {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
-        write!(f, "{}@{}:{}", self.name, self.addr, self.alias)
-    }
-}
-
-impl ::std::fmt::Display for datalog::SuccResult {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
-        write!(
-            f,
-            "{}@{}->{}~call={}",
-            self.name,
-            self.src,
-            self.dst,
-            self.call
-        )
-    }
-}
-
-impl ::std::fmt::Display for datalog::LinkPadResult {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
-        write!(f, "{}@{}: {}", self.name, self.addr, self.pad_name)
-    }
-}
-
-impl ::std::fmt::Display for datalog::GetFreeCallResult {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
-        write!(f, "{}@{}", self.name, self.addr)
-    }
-}
-
-impl ::std::fmt::Display for datalog::GetMallocCallResult {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
-        write!(f, "{}@{}", self.name, self.addr)
-    }
-}
-
-impl ::std::fmt::Display for datalog::LiveResult {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
-        write!(f, "{}@{}", self.name, self.addr)
-    }
-}
-
-impl ::std::fmt::Display for datalog::GetAliasResult {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
-        write!(
-            f,
-            "{}@{}:{} -> {}@{}:{} : {}",
-            self.file0,
-            self.addr0,
-            self.alias_set,
-            self.file,
-            self.addr,
-            self.a_var,
-            self.freed
-        )
-    }
-}
 
 pub fn uaf(files: &[String]) -> Database {
     let mut db = Database::new();
---
+++ src/printers.rs
@@ -0,0 +1,116 @@
+use std::fmt::{Display, Formatter, Result};
+use datalog::*;
+
+impl Display for FuncResult {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}@{}:{}", self.file, self.entry, self.addr)
+    }
+}
+
+impl Display for CallSiteResult {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(
+            f,
+            "{}@{}->{}@{}",
+            self.call_file,
+            self.call_addr,
+            self.dst_file,
+            self.dst_addr
+        )
+    }
+}
+
+impl Display for GetUafFlowResult {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}@{}:{}", self.name, self.addr, self.alias)
+    }
+}
+
+impl Display for SuccResult {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(
+            f,
+            "{}@{}->{}~call={}",
+            self.name,
+            self.src,
+            self.dst,
+            self.call
+        )
+    }
+}
+
+impl Display for LinkPadResult {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}@{}: {}", self.name, self.addr, self.pad_name)
+    }
+}
+
+impl Display for GetFreeCallResult {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}@{}", self.name, self.addr)
+    }
+}
+
+impl Display for GetMallocCallResult {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}@{}", self.name, self.addr)
+    }
+}
+
+impl Display for LiveResult {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}@{}", self.name, self.addr)
+    }
+}
+
+impl Display for GetAliasResult {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(
+            f,
+            "{}@{}:{} -> {}@{}:{} : {}",
+            self.file0,
+            self.addr0,
+            self.alias_set,
+            self.file,
+            self.addr,
+            self.a_var,
+            self.freed
+        )
+    }
+}
+
+impl Display for AnyFact {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match *self {
+            AnyFact::Lift(Lift {
+                              ref address,
+                              ref disassembly,
+                              ..
+                          }) => write!(f, "lift: {}: {:?}", address, disassembly),
+            AnyFact::Sym(Sym {
+                             ref name,
+                             ref start,
+                             ..
+                         }) => write!(f, "sym: {}@{}", name, start),
+            AnyFact::File(File { ref name, .. }) => write!(f, "file: {:?}", name),
+            AnyFact::Segment(Segment { ref start, ref end, .. }) => {
+                write!(f, "segment: {}->{}", start, end)
+            }
+            AnyFact::Live(Live { ref addr, .. }) => write!(f, "live: {}", addr),
+            AnyFact::Succ(Succ {
+                              ref dst_addr,
+                              ref src_addr,
+                              ..
+                          }) => write!(f, "succ: {} -> {}", src_addr, dst_addr),
+            AnyFact::SuccOver(SuccOver { ref dst, ref src, .. }) => {
+                write!(f, "succ_over: {} -> {}", src, dst)
+            }
+            AnyFact::ProgArch(ProgArch {
+                                  arch,
+                                  ref file_name,
+                              }) => write!(f, "arch: {}({})", file_name, arch),
+            // As a fallback, use debug
+            ref x => write!(f, "debug: {:?}", x),
+        }
+    }
+}
