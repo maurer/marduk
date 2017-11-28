5eff7baf57ae3efbbef4970728ddeb85deaa8ab5
Matthew Maurer <matthew.r.maurer@gmail.com>
2017-11-28 02:12:53 -0500

    Register cpio test spec

--- tests/whole.rs
+++ tests/whole.rs
@@ -13,10 +13,9 @@ fn check_uaf(names: &[&'static str], bugs: &[u64], max_fp: usize) {
     let ans = db.query_get_uaf_flow();
     // Make sure we found all the bugs
     for bug in bugs {
-        assert!(
-            ans.iter()
-                .any(|found| found.addr == BitVector::from_u64(*bug, 64))
-        );
+        assert!(ans.iter().any(|found| {
+            found.addr == BitVector::from_u64(*bug, 64)
+        }));
     }
 
     // Make sure we don't have more false positives than allowed
@@ -26,4 +25,9 @@ fn check_uaf(names: &[&'static str], bugs: &[u64], max_fp: usize) {
 #[test]
 fn gnome_nettool() {
     check_uaf(&["gnome-nettool"], &[0x41_14ab], 0);
+}
+
+#[test]
+fn bsdcpio_test() {
+    check_uaf(&["bsdcpio_test"], &[0x406305], 1);
 }
