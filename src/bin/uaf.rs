c13f4a2812fa7dc185bfa487cbc64225a190cb6e
Matthew Maurer <matthew.r.maurer@gmail.com>
2018-05-06 17:40:40 -0400

    Split running uaf from dumping debug information

--- src/bin/uaf.rs
+++ src/bin/uaf.rs
@@ -1,8 +1,6 @@
 extern crate env_logger;
 extern crate marduk;
 
-use std::time::{Duration, Instant};
-
 fn print_state(db: &mut marduk::datalog::Database) {
     println!("Steens:");
     for x in db.query_uaf() {
@@ -13,11 +11,6 @@ fn print_state(db: &mut marduk::datalog::Database) {
     for x in db.query_uaf_flow() {
         println!("{}", x);
     }
-
-    println!("PTS:");
-    for x in db.query_flow() {
-        println!("{}", x);
-    }
 }
 
 fn main() {
@@ -26,24 +19,6 @@ fn main() {
         &::std::env::args().collect::<Vec<_>>()[1..],
         marduk::AliasMode::All,
     );
-    let mut step = 0;
-    let mut last_round = Vec::new();
-    println!("Booting");
-    let total = Instant::now();
-    let timeout = Duration::new(60 * 60, 0); // 1 hr timeout
-    while (!last_round.is_empty() || step == 0) && total.elapsed() < timeout {
-        let mark = Instant::now();
-        last_round = db.run_rules_once();
-        step += 1;
-        println!(
-            "Step {} complete, took {:?}, {:?} total",
-            step,
-            mark.elapsed(),
-            total.elapsed()
-        );
-    }
+    db.run_rules();
     print_state(&mut db);
-    if !last_round.is_empty() {
-        println!("Run did not complete.");
-    }
 }
---
+++ src/bin/uaf_debug.rs
@@ -0,0 +1,48 @@
+extern crate env_logger;
+extern crate marduk;
+
+use std::time::Instant;
+
+fn print_state(db: &mut marduk::datalog::Database) {
+    println!("Steens:");
+    for x in db.query_uaf() {
+        println!("{}", x);
+    }
+
+    println!("Flow:");
+    for x in db.query_uaf_flow() {
+        println!("{}", x);
+    }
+
+    println!("PTS:");
+    for x in db.query_flow() {
+        println!("{}", x);
+    }
+}
+
+fn main() {
+    env_logger::init();
+    let mut db = marduk::uaf(
+        &::std::env::args().collect::<Vec<_>>()[1..],
+        marduk::AliasMode::All,
+    );
+    let mut step = 0;
+    let mut last_round = Vec::new();
+    println!("Booting");
+    let total = Instant::now();
+    while !last_round.is_empty() || step == 0 {
+        let mark = Instant::now();
+        last_round = db.run_rules_once();
+        step += 1;
+        println!(
+            "Step {} complete, took {:?}, {:?} total",
+            step,
+            mark.elapsed(),
+            total.elapsed()
+        );
+    }
+    print_state(&mut db);
+    if !last_round.is_empty() {
+        println!("Run did not complete.");
+    }
+}
